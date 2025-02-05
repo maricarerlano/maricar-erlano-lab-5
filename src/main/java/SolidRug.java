2e649543ef6b63907ab7df7f4b1fb9a0dda9d1e5
Maricar Erlano <maricarerlano@macbookair.myfiosgateway.com>
2025-02-04 23:20:03 -0500

    lab 5 done

---
+++ src/main/java/SolidRug.java
@@ -0,0 +1,27 @@
+public class SolidRug {
+    public static void main (String [] args){
+	    String [] [] emoji = new String [11] [11];
+	    for(int i = 0; i < emoji.length; i++){
+	        for(int j = 0; j < emoji[i].length; j++){
+	            emoji [i] [j] = "🌷";
+            }
+        }
+
+        print2DArray(emoji);
+
+    }
+
+
+    public static void print2DArray(String [] [] array){
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                System.out.print(array[i][j]);
+            }
+
+        System.out.println();
+
+        }
+
+    }
+
+}
