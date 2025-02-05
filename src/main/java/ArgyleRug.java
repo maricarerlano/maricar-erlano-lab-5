36f1e051355e3f6594d383a0901755f5f5d0c40c
Maricar Erlano <maricarerlano@macbookair.myfiosgateway.com>
2025-02-04 23:57:17 -0500

    rugs

--- src/main/java/ArgyleRug.java
+++ src/main/java/ArgyleRug.java
@@ -20,7 +20,7 @@ public class ArgyleRug {
     public static void print2DArray(String[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+                System.out.print(array[i][j]);
             }
             System.out.println();
         }
